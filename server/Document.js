cf4e90149bd9d7a7133b4a5af77cf0e96512495d
Rishabh Singh <rishabh15112001@gmail.com>
2023-10-06 03:15:15 +0530

    remove database, will add later

--- server/Document.js
+++
@@ -1,8 +0,0 @@
-import { Schema, model } from "mongoose";
-
-const Document = new Schema({
-  _id: String,
-  data: Object,
-});
-
-export default model("Document", Document);
--- server/server.js
+++ server/server.js
@@ -1,8 +1,4 @@
 import { Server } from "socket.io";
-import { mongoose } from "mongoose";
-import Document from "./Document.js";
-
-mongoose.connect("mongodb://localhost/scibo-docs");
 
 const io = new Server(3001, {
   cors: {
@@ -11,27 +7,13 @@ const io = new Server(3001, {
   },
 });
 
-const defaultValue = "";
-
 io.on("connection", (socket) => {
   socket.on("get-document", (documentId) => {
-    const document = findOrCreateDocument(documentId);
+    const data = "";
     socket.join(documentId);
-    socket.emit("load-document", document.data);
+    socket.emit("load-document", data);
     socket.on("send-changes", (delta) => {
       socket.broadcast.to(documentId).emit("receive-changes", delta);
     });
-
-    socket.on("save-document", async (data) => {
-      await Document.findByIdAndUpdate(documentId, { data });
-    });
   });
 });
-
-async function findOrCreateDocument(id) {
-  if (id == null) return;
-
-  const document = await Document.findById(id);
-  if (document) return document;
-  return await Document.create({ _id: id, data: defaultValue });
-}
