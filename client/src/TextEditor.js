1e8674b0182dc16ff89a0a9cfb59562f9038ff72
Rishabh Singh <rishabh15112001@gmail.com>
2023-10-06 00:09:22 +0530

    first commit

---
+++ client/src/TextEditor.js
@@ -0,0 +1,27 @@
+import React, { useCallback } from "react";
+import Quill from "quill";
+import "quill/dist/quill.snow.css";
+
+const TOOLBAR_OPTIONS = [
+  [{ header: [1, 2, 3, 4, 5, false] }],
+  [{ font: [] }],
+  [{ list: "ordered" }, { list: "bullet" }],
+  ["bold", "italic", "underline", "strike"],
+  [{ color: [] }, { background: [] }],
+  [{ script: "sub" }, { script: "super" }],
+  [{ indent: "-1" }, { indent: "+1" }],
+  [{ align: [] }],
+  ["image", "blockquote", "code-block", "image"],
+  ["clean"],
+];
+
+export default function TextEditor() {
+  const wrapperRef = useCallback((wrapper) => {
+    if (wrapper == null) return;
+    wrapper.innerHTML = "";
+    const editor = document.createElement("div");
+    wrapper.append(editor);
+    new Quill(editor, { theme: "snow", modules: { toolbar: TOOLBAR_OPTIONS } });
+  }, []);
+  return <div className="container" ref={wrapperRef}></div>;
+}
